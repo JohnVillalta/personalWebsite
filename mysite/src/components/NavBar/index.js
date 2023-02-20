d18ff57b98ba099aa56f40ca54337a25dd31b43d
JohnVillalta <jvillalta28007@gmail.com>
2023-02-19 22:25:05 -0800

    Changes to navbar in order to start stylizing homepage

--- mysite/src/components/NavBar/index.js
+++ mysite/src/components/NavBar/index.js
@@ -1,35 +1,38 @@
 import React from 'react';
-import { motion } from "framer-motion";
-import {
-    Nav,
-    NavLink,
-    Bars,
-    NavMenu,
-    NavBtn,
-    NavBtnLink
-} from './NavbarElements';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+    const LinkStyle = {
+        style : "text-[110%] text-[#496e46] align-middle rounded-full bg-[#fff]" + 
+        " px-[1.5rem] py-[0.8rem] h-[100%] hover:text-[#499443] transition-colors duration-500 ease-in-out"
+    };
+
+    const activeStyle = {
+        color: "#fff",
+        backgroundColor: "#496e46",
+        borderRadius: "40px",
+    };
+
     return (
-        <Nav id= "nav">
-            <NavMenu>
-                <NavLink to= '/home' activeStyle>
+        <nav className="h-[85px] flex justify-between m-0 py-[1rem] px-[31vw]">
+            <div className="flex align-middle mr-[-24px] w-[100vw] space-x-2">
+                <NavLink className={LinkStyle.style} to= '/home' style={({ isActive }) => isActive ? activeStyle : undefined }>
                     Home
                 </NavLink>
-                <NavLink to= '/contentcreation' activeStyle>
+                <NavLink className={LinkStyle.style} to= '/contentcreation' style={({ isActive }) => isActive ? activeStyle : undefined }>
                     Content Creation
                 </NavLink>
-                <NavLink to= '/programming' activeStyle>
+                <NavLink className={LinkStyle.style} to= '/programming' style={({ isActive }) => isActive ? activeStyle : undefined }>
                     Programming
                 </NavLink>
-                <NavLink to= '/music' activeStyle>
+                <NavLink className={LinkStyle.style} to= '/music' style={({ isActive }) => isActive ? activeStyle : undefined }>
                     Music
                 </NavLink>
-                <NavLink to= '/blogs' activeStyle>
+                <NavLink className={LinkStyle.style} to= '/blogs' style={({ isActive }) => isActive ? activeStyle : undefined }>
                     Blogs
                 </NavLink>
-            </NavMenu>
-        </Nav>
+            </div>
+        </nav>
     );
 };
 
--- mysite/src/pages/index.js
+++ mysite/src/pages/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className="border-2 border-[#496e46] m-2 p-2 h-100 flex-col">
-            <div className="flex-col justify-start">
-                <h1>John Villalta</h1>
+        <div className="border-2 border-[#496e46] m-2 p-2 flex">
+            <div className="flex-auto flex-col justify-start">
+                <h1 class="stripe_inner">John Villalta</h1>
                 <p>Aspiring Full Stack Developer</p>
                 <p>Lightweight Photographer</p>
                 <p>Music Hobbyist</p>
                 <p>Fitness Nut</p>
             </div>
-            <img src="../components/Images/profPic.png"></img>
-            <div className="flex justify-end">
-                <p className="w-60">There is too much demand for perfection and a lack of explicitness when seeking a career in tech. Why not spice up life with
-                     other interest? Check out mine!</p>
+            <img className="flex-1 rounded-full w-[40%] my-8"src={require('../components/Images/profPic.png')}/>
+            <div className="flex-auto">
+                <div className="flex items-end">
+                    <p className="w-60 text-justify bg-slate-500 ">There is too much demand for perfection and a lack of explicitness when seeking a career in tech. Why not spice up life with
+                        other interest? Check out mine!</p>
+                </div>
             </div>
         </div>
     );
