9d24986a3b9fe489437b86bf90d97ea9573bc5b1
JohnVill <jvillalta28007@gmail.com>
2023-05-31 10:00:57 -0700

    global updates

--- mysite/src/pages/contentcreation.js
+++ mysite/src/pages/contentcreation.js
@@ -5,69 +5,12 @@ import { Dialog, Transition } from '@headlessui/react';
 import ImageModal from '../components/ImgModal/imgModal'
 import CloseOut from '../components/ImgModal/closeout'
 import Image from '../components/image'
-<<<<<<< HEAD
-import toad from '../components/Images/toadhat.jpg'
-import zoro from '../components/Images/zoro.png'
-import flute from '../components/Images/flute.jpg'
-import sus from '../components/Images/sus.jpg'
-import link from '../components/Images/link.jpg'
-import joaquin from '../components/Images/joaquin.jpg'
-import cut from '../components/Images/cut.jpg'
-import { FaBorderNone } from 'react-icons/fa';
-import { Scrollbars } from "react-custom-scrollbars"
-=======
 import ImageNavBar from './imgbar'
->>>>>>> d18ff57b98ba099aa56f40ca54337a25dd31b43d
 
 const Content = ({propRef}) => {
     const [isShowing, setIsShowing] = useState(false);
     const [whichImg, setWhichImg] = useState(0);
 
-<<<<<<< HEAD
-    var [toggle, settoggle] = React.useState(0.5)
-    var imageDesc = ["This is description 1", "This is description 2", "This is description 3", "This is description 4", "This is description 5", "This is description 6", "This is description 7"]
-
-    const divStyle = {
-        textAlign: 'center', 
-        width: '100%',
-    };
-
-    function makeBlurry() {
-        if(toggle == 0.5 || document.getElementById("nav").style.filter == "none"){
-            settoggle(1);
-            document.getElementById("nav").style.filter = "blur(4px)";
-            document.getElementById("mainContentPage").style.filter = "blur(4px)";
-            document.getElementById("mainContentPage").style.overflow = "clip";
-        }else{
-            settoggle(0.5)
-            document.getElementById("nav").style.filter = "none";
-            document.getElementById("mainContentPage").style.filter = "none";
-            document.getElementById("mainContentPage").style.overflow = "scroll";
-        }
-    }
-
-    return (
-        <div>  
-            <div id="mainContentPage" style= {{divStyle}} onClick = {makeBlurry}>
-                <Link to='image1' activeStyle><Image add = {toad} text= 'This is image 1'/></Link>
-                <Link to='image2' activeStyle><Image add = {zoro} text= 'This is image 2'/></Link>
-                <Link to='image3' activeStyle><Image add = {flute} text= 'This is image 3'/></Link>
-                <Link to='image4' activeStyle><Image add = {sus} text= 'This is image 4'/></Link>
-                <Link to='image5' activeStyle><Image add = {link} text= 'This is image 5'/></Link>
-                <Link to='image6' activeStyle><Image add = {joaquin} text= 'This is image 6'/></Link>
-                <Link to='image7' activeStyle><Image add = {cut} text= 'This is image 7'/></Link>
-            </div>
-            
-            <Routes>
-                <Route path='image1' element={<ImageTemplate add = {toad} desc = {imageDesc[0]}/>} />
-                <Route path='image2' element={<ImageTemplate add = {zoro} desc = {imageDesc[1]}/>}/>
-                <Route path='image3' element={<ImageTemplate add = {flute} desc = {imageDesc[2]}/>}/>
-                <Route path='image4' element={<ImageTemplate add = {sus} desc = {imageDesc[3]}/>}/>
-                <Route path='image5' element={<ImageTemplate add = {link} desc = {imageDesc[4]}/>}/>
-                <Route path='image6' element={<ImageTemplate add = {joaquin} desc = {imageDesc[5]}/>}/>
-                <Route path='image7' element={<ImageTemplate add = {cut} desc = {imageDesc[6]}/>}/>
-            </Routes>
-=======
     function openModal() {
         propRef.current.classList.add("blurryBoi")
         setIsShowing(true);
@@ -88,7 +31,6 @@ const Content = ({propRef}) => {
             <CloseOut isShowing={isShowing} setIsShowing={setIsShowing} propRef={propRef}/> 
             <ImageModal add={imgAdds} text={imgDesc} date={imgDates} which={whichImg} isShowing={isShowing} setIsShowing={setIsShowing} propRef={propRef}/>
             <ImageNavBar isShowing={isShowing} setIsShowing={setIsShowing} id="PicBar" imgAdds={imgAdds} setWhichImg={setWhichImg}/>
->>>>>>> d18ff57b98ba099aa56f40ca54337a25dd31b43d
         </div>
     );
 };
--- mysite/src/pages/imgbar.js
+++ mysite/src/pages/imgbar.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import './imgbar.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import {NavLink as Link} from 'react-router-dom'
-import toad from '../components/Images/toadhat.jpg'
-import zoro from '../components/Images/zoro.png'
-import flute from '../components/Images/flute.jpg'
-import sus from '../components/Images/sus.jpg'
-import link from '../components/Images/link.jpg'
-import joaquin from '../components/Images/joaquin.jpg'
-import cut from '../components/Images/cut.jpg'
-import { Scrollbars } from "react-custom-scrollbars"
-
-const ImageNavBar = (props) => {
-    // var imageDesc = ["This is description 1", "This is description 2", "This is description 3"]
-=======
 import React, { Fragment } from 'react'
 import { Scrollbars } from 'react-custom-scrollbars'
 import { Dialog, Transition } from '@headlessui/react'
@@ -28,38 +11,12 @@ const ImageNavBar = ({ isShowing, setIsShowing, imgAdds, setWhichImg }) => {
         leaveFrom: 'translate-x-0',
         leaveTo: 'translate-x-full',
     };
->>>>>>> d18ff57b98ba099aa56f40ca54337a25dd31b43d
     
     function closeModal() {
         setIsShowing(false);
     }
 
     return (
-<<<<<<< HEAD
-        <div id="wrapDiv">
-            <Scrollbars style = {{width: "100%", height: "100%"}}>
-                <Link to = "../image1" activestyle><img class = "navImg" src = {toad}/></Link>
-                <Link to = "../image2" activestyle><img class = "navImg" src = {zoro}/></Link>
-                <Link to = "../image3" activestyle><img class = "navImg" src = {flute}/></Link>
-                <Link to = "../image4" activestyle><img class = "navImg" src = {sus}/></Link>
-                <Link to = "../image5" activestyle><img class = "navImg" src = {link}/></Link>
-                <Link to = "../image6" activestyle><img class = "navImg" src = {joaquin}/></Link>
-                <Link to = "../image7" activestyle><img class = "navImg" src = {cut}/></Link>
-
-                <Routes>
-                    <Route path='../image1'/>
-                    <Route path='../image2'/>
-                    <Route path='../image3'/>
-                    <Route path='../image4'/>
-                    <Route path='../image5'/>
-                    <Route path='../image6'/>
-                    <Route path='../image7'/>
-                </Routes>
-            </Scrollbars>
-        </div>
-
-        
-=======
         <Transition show={isShowing} as={Fragment} {...transitionVar}>
             <Dialog onClose={closeModal} className="fixed top-0 right-0 h-[100%] w-[500px]" id="wrapDiv">
                 <Scrollbars>
@@ -73,7 +30,6 @@ const ImageNavBar = ({ isShowing, setIsShowing, imgAdds, setWhichImg }) => {
                 </Scrollbars>
             </Dialog>
         </Transition>
->>>>>>> d18ff57b98ba099aa56f40ca54337a25dd31b43d
     );
 };
 
